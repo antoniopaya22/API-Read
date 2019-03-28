f8cd0ddf7434fe9f409d369b74a901330d93dba9
antonioalfa22 <antonioalfa22@gmail.com>
2019-03-28 18:47:22 +0100

    Cambiada url al .env

--- utils/dbConection.js
+++ utils/dbConection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports =  {
   conn:function(){
    
-    const mongoDB = 'mongodb://Chicago:Chicago22@ds123346.mlab.com:23346/chicago';
+    const mongoDB = process.env.MONGO_URL;
     mongoose.connect(mongoDB,{ useNewUrlParser: true });
     mongoose.Promise = global.Promise;
     mongoose.set('useCreateIndex', true)
