70b40c4ed1d9c0b37cbf163a06abb21aca15d76e
antonioalfa22 <antonioalfa22@gmail.com>
2019-03-28 17:06:11 +0100

    Cambiado puerto

--- utils/dbConection.js
+++ utils/dbConection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports =  {
   conn:function(){
    
-    const mongoDB = 'mongodb://Asturias:Asturias22@ds123346.mlab.com:23346/asturias';
+    const mongoDB = 'mongodb://Chicago:Chicago22@ds123346.mlab.com:23346/chicago';
     mongoose.connect(mongoDB,{ useNewUrlParser: true });
     mongoose.Promise = global.Promise;
     mongoose.set('useCreateIndex', true)
