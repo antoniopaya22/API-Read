6ea0a02360dc5099ca42ef849d659cec3e61abb6
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-15 12:34:40 +0200

    Modificados los test

--- modules/dBData.js
+++ modules/dBData.js
@@ -2,56 +2,22 @@ const Data = require('../models/data')
 
 module.exports = {
 
-
-    
-    //Get: 
-    //Return: all Data
     getAllData: function (Callback) {
-
-        Data.find({}, function (err, docs) {
-            
+        Data.find({}, function (err, docs) { 
             if (err) {
-                console.log("This is the error:" + err)
-                Callback(null);
+                Callback(err);
             }
             else {
                 console.log("data=",docs)
                 Callback(docs);
             }
         });
-
     },
-    //Get: starting time and ending time
-    //Return: all Data between those times
-    getDataBetweenTimes: function (timeStart, timeEnd, Callback) {
 
-        Data.find({$where: `parseInt(this.hour)<=${timeEnd} && parseInt(this.hour)>=${timeStart}`}, function (err, doc) {
-
-            if (err) {
-                console.log("This is the error:" + err)
-                Callback(null);
-            }
-            else {
-                if (doc) {
-                    Callback(doc);
-                }
-                else {
-                    Callback(null);
-                }
-            }
-
-        });
-    },
-    
-    //Get: data's id
-    //Return: 1 specific(id) Data information
     getDataById: function (id, Callback) {
-
         Data.find({ id: id }, function (err, doc) {
-
             if (err) {
-                console.log("This is the error:" + err)
-                Callback(null);
+                Callback(err);
             }
             else {
                 if (doc) {
@@ -61,7 +27,6 @@ module.exports = {
                     Callback(null);
                 }
             }
-
         });
     }
 
