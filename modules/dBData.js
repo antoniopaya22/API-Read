d63ddbd64201d531116dd58834ebb3f33d096698
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-22 12:52:04 +0200

    Añadida gestion de usuarios

--- modules/dBData.js
+++ modules/dBData.js
@@ -3,6 +3,7 @@ const Data = require('../models/data')
 module.exports = {
 
     getAllData: function (Callback) {
+        console.log(Data.host);
         Data.find({}, function (err, docs) { 
             if (err) {
                 Callback(err);
